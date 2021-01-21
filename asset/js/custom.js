a6160a6a4bbacb062fa1447c002c017f47fd12e2
devshisir <contact.shisirimran@gmail.com>
2021-01-22 03:24:42 +0600

    Version 2 implement fix

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -2,12 +2,14 @@ $(document).ready(function(){
 
     // this is video video play on hover and pause video on mouse leave
     $('.item_video').on('mouseenter',function(){
-        $(this).find('video').trigger('play'); // play video
+        // $(this).find('video').trigger('play'); // play video
+        $(this).find('video')[0].play(); // play video
     })
     
     $('.item_video').on('mouseleave',function(){
-        $(this).find('video').trigger('pause');// pause video
-        $(this).find('video').get(0).currentTime = 0; // rest video
+        // $(this).find('video').trigger('pause');// pause video
+        $('.item_video').find('video')[0].pause();// pause video
+        $('.item_video').find('video').get(0).currentTime = 0; // rest video
     })
 
 
